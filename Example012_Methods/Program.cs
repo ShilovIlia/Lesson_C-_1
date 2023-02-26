2d232e8e1842561420edc65b11ab070a4e237a15
Shilov Ilia <ilyashilov@bk.ru>
2023-02-26 18:59:44 +0300

    Сохранил урок №4

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+//Method1();
+
+//Вид 2
+void Method2 (string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2 ("Текст сообщения");
